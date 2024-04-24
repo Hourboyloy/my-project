8e4f0c2bf4f6b812fa3437b328138abb1af1bf0e
Hourboyloy <chhoysienghour@gmail.com>
2024-04-25 00:51:11 +0700

    edit

--- src/App.js
+++ src/App.js
@@ -7,10 +7,17 @@ function App() {
     setCatchValueInput(value);
   };
   const [trueFalse,setTrueFalse] = useState(false)
-  setTimeout(()=>{setTrueFalse(true);},10000)
+  setTimeout(()=>{setTrueFalse(true);},5000)
   return (
     <div className="App relative space-y-0 bg-gray-900 ">
       <div>
+        {trueFalse ? (
+          ""
+        ) : (
+          <div className=" h-screen flex justify-center items-center bg-white">
+            <div className="loader rounded"></div>
+          </div>
+        )}
         {trueFalse ? (
           <div className=" ">
             <Header
@@ -19,16 +26,15 @@ function App() {
             />
           </div>
         ) : (
-          <div className=" h-screen flex justify-center items-center bg-white">
-            <div className="loader rounded"></div>
+          <div className=" w-0 h-0 overflow-hidden">
+            <Header
+              catchValueInput={catchValueInput}
+              handleCatchValueInput={handleCatchValueInput}
+            />
           </div>
         )}
       </div>
-      <div>
-        {
-          trueFalse?<Outlet />:""
-        }
-      </div>
+      <div>{trueFalse ? <Outlet /> : ""}</div>
     </div>
   );
 }
